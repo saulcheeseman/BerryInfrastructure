9d69f5c922163dae514707e1c8e80677820ed87e
saulcheeseman <75391793+wumbomumbo@users.noreply.github.com>
2025-01-02 01:51:37 +0000

    find city api for weather, crashes for whatever reason

---
+++ widget/rimbb10/city-find.asp.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-Type: text/xml; charset=utf-8");
+?>
+<adc_database>
+    <citylist us="10" intl="11" extra_cities="0">
+        <location cnt="1" city="London" state="AR" location="72847" />
+        <location cnt="2" city="London" state="KY" location="40741" />
+        <location cnt="3" city="London" state="OH" location="43140" />
+        <location cnt="4" city="London" state="TX" location="76854" />
+        <location cnt="5" city="London" state="WV" location="25126" />
+        <location cnt="6" city="London Corbin Arpt Magee Field" state="KY" location="AA407" />
+        <location cnt="7" city="London Mills" state="IL" location="61544" />
+        <location cnt="8" city="Londonderry" state="NH" location="03053" />
+        <location cnt="9" city="Londonderry" state="OH" location="45647" />
+        <location cnt="10" city="Londonderry" state="VT" location="05148" />
+        <location cnt="1" city="London" state="United Kingdom(Westminster, England)" location="EUR|UK|UK241|LONDON" />
+        <location cnt="2" city="London" state="Canada(Ontario)" location="NAM|CA|ON|LONDON" />
+        <location cnt="3" city="London" state="Belize(Orange Walk)" location="CAC|BZ|BH004|LONDON" />
+        <location cnt="4" city="London" state="Equatorial Guinea" location="AFR|GQ|EK000|LONDON" />
+        <location cnt="5" city="London" state="Kiribati" location="OCN|KI|KR000|LONDON" />
+        <location cnt="6" city="London" state="Burma/Myanmar(Kachin State)" location="ASI|MM|BM004|LONDON" />
+        <location cnt="7" city="London" state="Nigeria" location="AFR|NG|NI000|LONDON" />
+        <location cnt="8" city="London" state="Philippines(General Santos)" location="ASI|PH|RP049|LONDON" />
+        <location cnt="9" city="London" state="Russia(Altai Mountains)" location="ASI|RU|RS003|LONDON" />
+        <location cnt="10" city="London" state="South Africa(Northern Province)" location="AFR|ZA|SF009|LONDON" />
+        <location cnt="11" city="London" state="South Africa(Mpumalanga)" location="AFR|ZA|SF006|LONDON" />
+    </citylist>
+    <copyright>Copyright 2013 AccuWeather.com</copyright>
+    <use>
+        This document is intended only for use by authorized licensees of AccuWeather.com. Unauthorized use is prohibited. All Rights Reserved.
+    </use>
+    <product>rimbb10</product>
+    <redistribution>Redistribution Prohibited.</redistribution>
+</adc_database>
